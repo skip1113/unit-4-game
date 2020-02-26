3880e5232cc261313506fd260dd4d2ad5e0ee766
Skip Khamvongsa <skipk95@gmail.com>
2020-02-25 17:20:06 -0700

    Added each crystal a click and random number function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,105 @@
  //variable to hold the shown number
  var compNumber = "";
+ //Adding global counter variable
+ var counter = 0;
 //loop to generate the shown number between 19-120.
 var shownNum = Math.floor(19 + Math.random() * 102);
 //Display the generated number on the page
 $("#totalNum").append(shownNum);
-
+//variables for wins and losses.
+var wins = 0;
+var losses = 0;
 //array for gems 1-12
-var gemArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-//adding classes to each gem
-// $("#crystal-one").attr("data-crystalvalue", gemArray[i]);
-// $("#crystal-two").attr("data-crystalvalue", gemArray[i]);
-// $("#crystal-three").attr("data-crystalvalue", gemAray[i]);
-// $("#crystal-four").attr("data-crystalvalue", gemArray[i]);
-//loop for the array of numbers for the crystals
-for (var i = 0; i < gemArray.length; i++) {
+var gemArray = "";
+//adding text content for score, wins and losses
+scoreText = document.getElementById("score");
+winsText = document.getElementById("winner");
+lossesText = document.getElementById("loser");
+//variables for html crystals
     var crystalOne = $(".crystal-one");
     var crystalTwo = $(".crystal-two");
     var crystalThree = $(".crystal-three");
     var crystalFour = $(".crystal-four");
-    crystalOne.attr("data-crystalvalueone", gemArray[i]);
-    crystalTwo.attr("data-crystalvaluetwo", gemArray[i]);
-    crystalThree.attr("data-crystalvaluethree", gemArray[i]);
-    crystalFour.attr("data-crystalvaluefour", gemArray[i]);
-    //crystals given class
-    crystalOne.addClass("crystal-image");
-    crystalTwo.addClass("crystal-image");
-    crystalThree.addClass("crystal-image");
-    crystalFour.addClass("crystal-image");
-       
+    var greenCry = Math.floor(Math.random() * 12) + 1;
+    var pinkCry = Math.floor(Math.random() * 12) + 1;
+    var purpCry = Math.floor(Math.random() * 12) + 1;
+    var blueCry = Math.floor(Math.random() * 12) + 1;
+//reset game Function
+function gameReset() {
+    counter = 0;
+    $("#score").text(counter);
+
+    resetNum = Math.floor(19 + Math.random() * 102);
+    $("#totalNum").text(resetNum);
+
+    greenCry = Math.floor(Math.random() * 12) + 1;
+    pinkCry = Math.floor(Math.random() * 12) + 1;
+    purpCry = Math.floor(Math.random() * 12) + 1;
+    blueCry = Math.floor(Math.random() * 12) + 1;
+    
 }
-$(function() {
-    $(".crystal-image").on("click", function() {
-        
-        alert("yeet");
+//click functions for each crystal
 
-        var crystalValueOne = ($(this).attr("data-crystalvalueone"));
+    $(".crystal-one").on("click", function() {
+        counter += greenCry;
         
+        // console.log("New score " + greenCry);
+        $("#score").text(counter);
+        // scoreText.textContent = counter;
 
-        console.log(crystalValueOne);
+        if (counter === shownNum) {
+            wins += 1;
+            $("#winner").text(wins);
+            // winsText.textContent = wins;
+            gameReset();
+        } else if (counter >= shownNum) {
+            losses += 1;
+            $("#loser").text(losses);
+            // lossesText.textContent = losses;
+            gameReset();
+            //.text can change/update your html with id's try that instead of .textContent
+        }
+    }) 
+    $(".crystal-two").on("click", function() {
+        counter += pinkCry;
+        $("#score").text(counter);
+        if (counter === shownNum) {
+            wins += 1;
+            $("#winner").text(wins);
+            gameReset();
+        } else if (counter >= shownNum) {
+            losses += 1;
+            $("#loser").text(losses);
+            gameReset();
+            
+        }
     })
-});
+    $(".crystal-three").on("click", function() {
+        counter += purpCry;
+        $("#score").text(counter);
+        if (counter === shownNum) {
+            wins += 1;
+            $("#winner").text(wins);
+            gameReset();
+        } else if (counter >= shownNum) {
+            losses += 1;
+            $("#loser").text(losses);
+            gameReset();
+            
+        }
+    })
+    $(".crystal-four").on("click", function() {
+        counter += blueCry;
+        $("#score").text(counter);
+        if (counter === shownNum) {
+            wins += 1;
+            $("#winner").text(wins);
+            gameReset();
+        } else if (counter >= shownNum) {
+            losses += 1;
+            $("#loser").text(losses);
+            gameReset();
+            
+        }
+    })
+
